2da3042941c9372f4c55f56e9c4b9a3ee0579ee6
dhivakar0227 <dhivakar0227@gmail.com>
2020-09-04 11:24:41 -0700

    all files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1> Hello World! </h1>
+  </div>
+</template>
+
+<script>
+import questionsAdder from "./components/Questions";
+export default {
+  components: {
+    communityQuestionsadder: questionsAdder
+  }
+}
+</script>
+
+<style>
+
+</style>
